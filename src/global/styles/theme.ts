6ba7aee40d5b1c89e69749a4a5c41908ed2819d8
jeziel <jezielmarques7@gmail.com>
2021-10-20 04:18:48 -0300

    create project theme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,32 @@
+export const theme = {
+  colors: {
+    primary: '#1d2542',
+    background: '#F0F2F5',
+
+    secondary100: '#0D1033',
+    secondary90: '#0D133D',
+    secondary80: '#0D1647',
+    secondary75: '#171F52',
+    secondary70: '#1B2565',
+    secondary60: '#1B2565',
+    secondary50: '#243189',
+    secondary40: '#1D2766',
+    secondary30: '#495BCC',
+
+    overlay: 'rgba(0,0,0,0.7)',
+    highlight: '#ABB1CC',
+    heading: '#DDE3F0',
+    title: '#363F5F',
+    text: '#969CB2',
+    shape: '#FFFF',
+    red: '#E83F5B',
+    blue: '#5636D3',
+    green: '#04d361',
+    orange: '#FF872C',
+  },
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold',
+  },
+};
